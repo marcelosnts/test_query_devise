ab08c7811952eb3f28f88f386a5653550bc8be19
Marcelo <santosmarcelo1995@live.com>
2021-11-07 14:06:02 -0300

    Add areas table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_07_161010) do
+ActiveRecord::Schema.define(version: 2021_11_07_170454) do
+
+  create_table "areas", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
