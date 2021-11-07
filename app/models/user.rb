2b55e7461c7452d4e91eeb6b58ee66bd0c945811
Marcelo <santosmarcelo1995@live.com>
2021-11-07 14:57:28 -0300

    Add optional true

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  belongs_to :area
+  belongs_to :area, optional: true
 end
